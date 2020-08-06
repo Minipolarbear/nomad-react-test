f6c44e14b42064295802a36be3be45e6e11dc6d0
Jin-Yeon Kim <jy.kim@4cgate.com>
2020-08-06 10:36:24 +0900

    Updates

---
+++ precache-manifest.b02e6370a98925cd03ac82ec74fe5666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10faaec66b2328a0756499425d3d3e8e",
+    "url": "/nomad-react-test/index.html"
+  },
+  {
+    "revision": "02ee00023f5085c59580",
+    "url": "/nomad-react-test/static/css/main.09624fb0.chunk.css"
+  },
+  {
+    "revision": "71b5504ba4c0a0b0939d",
+    "url": "/nomad-react-test/static/js/2.5b76a9db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad-react-test/static/js/2.5b76a9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ee00023f5085c59580",
+    "url": "/nomad-react-test/static/js/main.811b1098.chunk.js"
+  },
+  {
+    "revision": "d6fd438492982e7fef63",
+    "url": "/nomad-react-test/static/js/runtime-main.70395cbe.js"
+  }
+]);
